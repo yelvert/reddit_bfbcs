b32f8557b96cb345c9467f55c0266d04dfc9ef30
Taylor Yelverton <taylor@synergy-solutions.biz>
2011-01-07 15:22:48 -0600

    Applied Rails3Setup template.

---
+++ Gemfile
@@ -0,0 +1,17 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.1'
+
+gem 'sqlite3-ruby', :require => 'sqlite3'
+
+gem 'devise', :git => 'http://github.com/plataformatec/devise.git'
+gem 'syntax'
+
+group :development, :test do
+  gem 'rspec-rails'
+  gem 'steak', '>= 1.0.0.rc.1'
+  gem 'capybara'
+  gem 'database_cleaner'
+  gem 'launchy'
+  gem 'fabrication'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RedditBFBC2::Application.config.secret_token = 'e861c6b3cb7b4547babee3a91517ab42bd9608ae7f513f7e305b1f9da68e1da46324b91335d339c5dc8908e0b28071e9f66908758b1b74e42fc3638486ed5a24'
